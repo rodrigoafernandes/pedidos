2620e14f2786476dfe9aad4c7072fd4b0bcbf87d
Rodrigo Fernandes <rodrigoforit@gmail.com>
2019-12-13 23:14:42 -0300

    Adicionado classes de config para os testes unitarios

---
+++ src/test/java/br/com/gerenciador/pedidos/config/MariaDBTestConfig.java
@@ -0,0 +1,45 @@
+package br.com.gerenciador.pedidos.config;
+
+import ch.vorburger.exec.ManagedProcessException;
+import ch.vorburger.mariadb4j.DBConfigurationBuilder;
+import ch.vorburger.mariadb4j.springframework.MariaDB4jSpringService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("test-db")
+public class MariaDBTestConfig {
+
+    @Value("${gerenciador.pedidos.database.name}")
+    private String databaseName;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String datasourceDriver;
+
+    @Bean
+    public MariaDB4jSpringService mariaDB4jSpringService() {
+        return new MariaDB4jSpringService();
+    }
+
+    @Bean
+    public DataSource dataSource() throws ManagedProcessException {
+        mariaDB4jSpringService().getDB().createDB(databaseName);
+
+        DBConfigurationBuilder config = mariaDB4jSpringService().getConfiguration();
+
+        return DataSourceBuilder.create().username(datasourceUsername).password(datasourcePassword)
+                .url(config.getURL(databaseName)).driverClassName(datasourceDriver).build();
+    }
+
+}
---
+++ src/test/java/br/com/gerenciador/pedidos/repository/impl/PedidoRepositoryTestImpl.java
@@ -0,0 +1,51 @@
+package br.com.gerenciador.pedidos.repository.impl;
+
+import br.com.gerenciador.pedidos.dto.FiltroPedidosDTO;
+import br.com.gerenciador.pedidos.dto.ItemPedidoDTO;
+import br.com.gerenciador.pedidos.dto.PedidoDTO;
+import br.com.gerenciador.pedidos.repository.PedidoRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.StringUtils.isNoneBlank;
+import static org.apache.commons.lang3.math.NumberUtils.*;
+
+public class PedidoRepositoryTestImpl implements PedidoRepository {
+
+    private List<PedidoDTO> pedidos = new ArrayList<>();
+
+    public PedidoRepositoryTestImpl() {
+        pedidos.addAll(asList(PedidoDTO.builder().cliente("Cliente Teste").dataCadastro(LocalDate.now())
+                .numPedido(LONG_ONE).valorTotal(createBigDecimal("100.37"))
+                .itens(asList(ItemPedidoDTO.builder().nomeItem("Item 1").quantidade(LONG_ONE)
+                        .valorUnitario(createBigDecimal("100.37")).build())).build()));
+    }
+
+    @Override
+    public List<PedidoDTO> findByFilter(FiltroPedidosDTO filtro) {
+        List<PedidoDTO> results = pedidos;
+        if (filtro.getNumeroPedido() != null && filtro.getNumeroPedido().compareTo(LONG_ZERO) > INTEGER_MINUS_ONE) {
+            results = results.stream().filter(pedido -> pedido.getNumPedido().equals(filtro.getNumeroPedido()))
+                    .collect(toList());
+        }
+
+        if (filtro.getDataCadastro() != null) {
+            results = results.stream().filter(pedido -> pedido.getDataCadastro().equals(filtro.getDataCadastro()))
+                    .collect(toList());
+        }
+
+        if (isNoneBlank(filtro.getNomeCliente())) {
+            results = results.stream().filter(pedido -> pedido.getCliente().equalsIgnoreCase(filtro.getNomeCliente()))
+                    .collect(toList());
+        }
+        return results;
+    }
+}
