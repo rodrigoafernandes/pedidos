5ad58b503e6c448ee5ead9c38bba7e1b2b5bc8ce
Rodrigo Fernandes <rodrigoforit@gmail.com>
2019-12-15 08:51:38 -0300

    testes unitarios na camada service

--- src/test/java/br/com/gerenciador/pedidos/repository/impl/PedidoRepositoryTestImpl.java
+++ src/test/java/br/com/gerenciador/pedidos/repository/impl/PedidoRepositoryTestImpl.java
@@ -3,9 +3,11 @@ package br.com.gerenciador.pedidos.repository.impl;
 import br.com.gerenciador.pedidos.dto.FiltroPedidosDTO;
 import br.com.gerenciador.pedidos.dto.ItemPedidoDTO;
 import br.com.gerenciador.pedidos.dto.PedidoDTO;
+import br.com.gerenciador.pedidos.exception.ItensPedidoNotFoundException;
 import br.com.gerenciador.pedidos.exception.PedidosNotFoundException;
 import br.com.gerenciador.pedidos.repository.PedidoRepository;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 import static org.apache.commons.lang3.StringUtils.isNoneBlank;
 import static org.apache.commons.lang3.math.NumberUtils.*;
 
@@ -24,7 +27,9 @@ public class PedidoRepositoryTestImpl implements PedidoRepository {
         pedidos.addAll(asList(PedidoDTO.builder().cliente("Cliente Teste").dataCadastro(LocalDate.now())
                 .numPedido(LONG_ONE).valorTotal(createBigDecimal("100.37"))
                 .itens(asList(ItemPedidoDTO.builder().nomeItem("Item 1").quantidade(LONG_ONE)
-                        .valorUnitario(createBigDecimal("100.37")).build())).build()));
+                        .valorUnitario(createBigDecimal("100.37")).build())).build(),
+                PedidoDTO.builder().cliente("Cliente Teste 2")
+                        .dataCadastro(LocalDate.now().plusDays(createLong("3"))).numPedido(createLong("3")).build()));
     }
 
     @Override
@@ -45,9 +50,15 @@ public class PedidoRepositoryTestImpl implements PedidoRepository {
                     .collect(toList());
         }
 
-        if (CollectionUtils.isEmpty(results)) {
+        if (isEmpty(results)) {
             throw new PedidosNotFoundException("Pedidos nao localizados para o filtro informado");
         }
+
+        results.forEach(pedido -> {
+            if (isEmpty(pedido.getItens())) {
+                throw new ItensPedidoNotFoundException("Itens não encontrados para o pedido");
+            }
+        });
 
         return results;
     }
---
+++ src/test/java/br/com/gerenciador/pedidos/service/PedidoServiceTests.java
@@ -0,0 +1,90 @@
+package br.com.gerenciador.pedidos.service;
+
+import br.com.gerenciador.pedidos.converter.ParametersConverter;
+import br.com.gerenciador.pedidos.dto.FiltroPedidosDTO;
+import br.com.gerenciador.pedidos.dto.PedidoDTO;
+import br.com.gerenciador.pedidos.exception.InvalidParametersException;
+import br.com.gerenciador.pedidos.repository.PedidoRepository;
+import br.com.gerenciador.pedidos.validator.ParametersValidator;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.hibernate.validator.internal.engine.ConstraintViolationImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.math.NumberUtils.LONG_MINUS_ONE;
+import static org.apache.commons.lang3.math.NumberUtils.LONG_ONE;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@ExtendWith(MockitoExtension.class)
+public class PedidoServiceTests {
+
+    @Mock
+    PedidoRepository pedidoRepository;
+
+    @Mock
+    ParametersConverter parametersConverter;
+
+    @Mock
+    ParametersValidator validator;
+
+    @InjectMocks
+    private PedidoServices pedidoServices;
+
+    @BeforeEach
+    public void setup() {
+        initMocks(this);
+    }
+
+    @Test
+    public void givenInvalidRequestParameters_whenFindPedidos_thenShouldThrowsInvalidParametersException() {
+        when(parametersConverter.convert(anyString(), anyString(), anyString(),any(Set.class))).thenCallRealMethod();
+
+        assertThrows(InvalidParametersException.class, () -> {
+           pedidoServices.findPedidos("AAA", "15/12/2019", EMPTY);
+        });
+
+    }
+
+    @Test
+    public void givenInvalidConvertedParameters_whenFindPedidos_thenShouldThrowsInvalidParametersException() {
+        when(parametersConverter.convert(anyString(), anyString(), anyString(), any(Set.class)))
+                .thenReturn(FiltroPedidosDTO.builder().numeroPedido(LONG_MINUS_ONE).build());
+        Mockito.doCallRealMethod().when(validator).validate(any(FiltroPedidosDTO.class), any(Set.class));
+        assertThrows(InvalidParametersException.class, () -> {
+            pedidoServices.findPedidos("-1", EMPTY, EMPTY);
+        });
+    }
+
+    @Test
+    public void givenValidParameters_whenFindPedidos_thenShouldReturnsListOfPedidosDTO() {
+        when(parametersConverter.convert(anyString(), anyString(), anyString(), any(Set.class)))
+                .thenReturn(FiltroPedidosDTO.builder().numeroPedido(LONG_ONE).build());
+        doNothing().when(validator).validate(any(FiltroPedidosDTO.class), any(Set.class));
+        when(pedidoRepository.findByFilter(any(FiltroPedidosDTO.class)))
+                .thenReturn(asList(PedidoDTO.builder().build()));
+        assertTrue(isNotEmpty(pedidoServices.findPedidos("1", EMPTY, EMPTY)));
+    }
+
+}
